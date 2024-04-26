ac105bbff8f10b0e89d253bd295afccf46fc491c
Oliver Gorgiev <oliver.gorgiev11@gmail.com>
2024-04-26 19:38:02 +0200

    first push

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,21 @@
+import classes from './CartButton.module.css';
+import { uiActons } from '../../store/ui-slice';
+import { useDispatch, useSelector } from 'react-redux';
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector((state) => state.cart.totalQuantity);
+
+  const toggleCartHandler = () => {
+     dispatch(uiActons.toggle());
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
