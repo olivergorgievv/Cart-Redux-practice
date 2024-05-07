d1c9af671b6721f71ec1bdb6952b83b2f602686c
Oliver <oliver.gorgiev11@gmail.com>
2024-05-07 17:41:43 +0200

    using firebase to send API Requests inside the app component when item is added to the cart,using redux toolking to set the state

--- src/App.js
+++ src/App.js
@@ -1,18 +1,73 @@
-import { useSelector } from 'react-redux';
+import { useSelector, useDispatch } from 'react-redux';
 
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import { useEffect } from 'react';
+import { uiActons } from './store/ui-slice';
+import Notification from './components/UI/Notification';
+
+let isInitial = true;
 
 function App() {
 
   const showCart = useSelector(state => state.ui.cartIsVisible);
+  const cart = useSelector(state => state.cart);
+  const dispatch = useDispatch();
+  const notification = useSelector(state => state.ui.notification)
+
+  useEffect(() => {
+    const sendCartData = async() => {
+      dispatch(uiActons.showNotification({
+        status: 'pending',
+        title: 'Sending..',
+        message: 'Sending cart Data'
+      })
+    );
+      const response = await fetch('https://advanced-redux-98735-default-rtdb.europe-west1.firebasedatabase.app/cart.json', 
+      { method: 'PUT', 
+      body: JSON.stringify(cart)
+    });
+    if (!response.ok){
+     throw new Error('Sending cart data failed.')
+    }
+
+    dispatch(uiActons.showNotification({
+      status: 'success',
+      title: 'Success!',
+      message: 'Sent cart data succesfully'
+    })
+  );
+};
+
+if(isInitial) {
+  isInitial = false;
+  return;
+}
+ sendCartData().catch(error => {
+  dispatch(
+    uiActons.showNotification({
+      status: 'error',
+      title: 'Error!',
+      message: 'Sending cart data failed!',
+    })
+  )
+ })
+},[cart, dispatch]);
 
   return (
+    <>
+    {notification && 
+    <Notification 
+    status={notification.status}
+    title={notification.title}
+    message={notification.message}
+    />}
     <Layout>
       {showCart && <Cart />}
       <Products />
     </Layout>
+    </>
   );
 }
 
